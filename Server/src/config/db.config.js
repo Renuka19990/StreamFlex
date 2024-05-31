c1ad747998d37f5243771d5c785914f33637e70b
renuka <renukasjagrwal@gmail.com>
2024-05-31 16:02:11 +0530

    resolved issues

---
+++ Server/src/config/db.config.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.HOST,
+  port: process.env.DBPORT,
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+    connectTimeout: 86400
+  },
+});
+
+module.exports = sequelize;
+
+  async function ConnectionToDB(){
+    try{
+        await sequelize.authenticate();
+        console.log('Connection to the database has been established successfully.');
+    }
+    catch(error){
+        console.error('Unable to connect to the database:', error);
+    }
+  }
+  module.exports={
+    ConnectionToDB,
+    sequelize
+};
